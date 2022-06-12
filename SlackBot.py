ab8790d9f9abb7ff4f25d35845da295717e551b0
Neria Nabeth <nnabeth@gmail.com>
2022-06-12 11:36:26 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	botTem.py

--- SlackBot.py
+++ SlackBot.py
@@ -1,49 +1,20 @@
-import slack
+import flask
 import os
-from pathlib import Path
-from dotenv import load_dotenv
-from flask import Flask
-from slackeventsapi import SlackEventAdapter
-import urllib.request, json
+from flask import send_from_directory
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
+app = flask.Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/favicon.png')
 
-zipCode = "281184"
-f = open("citites.txt", "r")
-amap = ["one",""]
-map = f.read().replace("\n",',').split(",")
-
-
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
-
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-
-# client.chat_postMessage(channel='#test', text="Hello World!")
-
-BOT_ID = client.api_call("auth.test")['user_id']
-
-@slack_event_adapter.on('message')
-def message(payload):
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-
-        # return
-    if BOT_ID != user_id:
-        for city in map:
-            if text.lower() in city.lower():
-                zipCode = city.split("|")[-1]
-                print(city)
-                with urllib.request.urlopen(
-                        "https://www.hebcal.com/shabbat?cfg=json&geonameid=" + zipCode + "&M=on&lg=h") as url:
-                    data = json.loads(url.read().decode())
-                    data = "הדלקת נרות, שבת " + data.get("items")[0].get("memo")+" ב- " +data.get("location").get("title")+" תכנס ב- " + data.get("items")[0].get("title").split(": ")[-1]
-                    client.chat_postMessage(channel=channel_id,text=data)
-
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Hello World"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
