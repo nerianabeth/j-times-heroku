51d34566a93439debde40b6bdf90ec594ff25760
Neria Nabeth <nnabeth@gmail.com>
2022-06-17 17:17:23 +0300

    slack

--- SlackBot.py
+++ SlackBot.py
@@ -29,6 +29,25 @@ client.chat_postMessage(channel='#test', text="Hello World!")
 
 BOT_ID = client.api_call("auth.test")['user_id']
 
+@slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+
+        # return
+    if BOT_ID != user_id:
+        for city in map:
+            if text.lower() in city.lower():
+                zipCode = city.split("|")[-1]
+                print(city)
+                with urllib.request.urlopen(
+                        "https://www.hebcal.com/shabbat?cfg=json&geonameid=" + zipCode + "&M=on&lg=h") as url:
+                    data = json.loads(url.read().decode())
+                    data = "הדלקת נרות, שבת " + data.get("items")[0].get("memo")+" ב- " +data.get("location").get("title")+" תכנס ב- " + data.get("items")[0].get("title").split(": ")[-1]
+                    client.chat_postMessage(channel=channel_id,text=data)
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
@@ -37,7 +56,7 @@ def favicon():
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello World0.02"
+    return "Hello World1.0"
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
