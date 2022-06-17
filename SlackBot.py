2530b44ede1bad45cb2de2fe34f90abbdd9bf8f2
Neria Nabeth <nnabeth@gmail.com>
2022-06-17 18:05:44 +0300

    slack

--- SlackBot.py
+++ SlackBot.py
@@ -25,7 +25,28 @@ slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/ev
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 
-client.chat_postMessage(channel='#test', text="Hello World!")
+# client.chat_postMessage(channel='#test', text="Hello World!")
+
+BOT_ID = client.api_call("auth.test")['user_id']
+
+@slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+
+        # return
+    if BOT_ID != user_id:
+        for city in map:
+            if text.lower() in city.lower():
+                zipCode = city.split("|")[-1]
+                print(city)
+                with urllib.request.urlopen(
+                        "https://www.hebcal.com/shabbat?cfg=json&geonameid=" + zipCode + "&M=on&lg=h") as url:
+                    data = json.loads(url.read().decode())
+                    data = "הדלקת נרות, שבת " + data.get("items")[0].get("memo")+" ב- " +data.get("location").get("title")+" תכנס ב- " + data.get("items")[0].get("title").split(": ")[-1]
+                    client.chat_postMessage(channel=channel_id,text=data)
 
 @app.route('/favicon.ico')
 def favicon():
@@ -35,7 +56,7 @@ def favicon():
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello World0.04"
+    return "Hello World0.14"
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
