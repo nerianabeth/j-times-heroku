20412373c7cf2a112609c603e6ba460d25d15ea3
Neria Nabeth <nnabeth@gmail.com>
2022-06-17 17:07:45 +0300

    slack

--- SlackBot.py
+++ SlackBot.py
@@ -4,23 +4,19 @@ import slack
 from pathlib import Path
 from dotenv import load_dotenv
 from flask import send_from_directory
-from slackeventsapi import SlackEventAdapter
 import urllib.request, json
 
 app = flask.Flask(__name__)
-
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/favicon.png')
 
-
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello World0.1"
-
+    return "Hello World0.01"
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
